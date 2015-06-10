1082c8552002609e7d8e19bcc1f6ae8531fa9261
Christopher Carlson <christopher.s.carlson@wmich.edu>
2015-06-10 19:54:13 -0400

    Really Finished

--- A2/Iris_Data.R
+++ A2/Iris_Data.R
@@ -11,7 +11,7 @@
   n_test = floor((1/4)*nrow(iris))
   n_train = nrow(iris) - n_test
 
-  set.seed(100) # This is required in order to make the sampling results
+  set.seed(431) # This is required in order to make the sampling results
 							# reproducable.
 
  # Rather than sampling directly from the data, it is easier to set up
@@ -27,34 +27,37 @@
  
  # Using the vglm function for multinomial categorical data is a bit 
  # different than we are used to.  This link is an excellent reference
- # for the lglm function: https://www.stat.auckland.ac.nz/~yee/VGAM/doc/VGAMrefcard.pdf
-  print("Logistic Regression")
+ # for it: https://www.stat.auckland.ac.nz/~yee/VGAM/doc/VGAMrefcard.pdf
+  print('-----------------------------')
+  print("LOGISTIC REGRESSION")
   
  
   
   glm.model=vglm(Species~cbind(Sepal.Length, Sepal.Width, Petal.Length,
 					Petal.Width), family=multinomial, data=iris_train)
   glm.prob=as.data.frame(predict(glm.model, iris_test, type="response"))
-  glm.pred=rep("Iris-virginica", nrow(iris_test))
-  glm.pred[glm.prob$setosa > 0.66] = "Iris-setosa"
-  glm.pred[glm.prob$versicolor > 0.66] = "Iris-versicolor"
- 
+ # For reasons I don't understand, predict has to be casted as a data-
+ # frame.  Without the cast it returns an 'atomic object'.
+  glm.pred=rep("virginica", nrow(iris_test))
+  glm.pred[glm.prob$setosa > 0.66] = "setosa"
+  glm.pred[glm.prob$versicolor > 0.66] = "versicolor"
   table(glm.pred, iris_test$Species)
-
+  mean(glm.pred==iris_test$Species)
 
  ### Linear Discriminant Analysis ###
+  print('-----------------------------')
   print("LINEAR DISCRIMINANT ANALYSIS")
 
   lda.model=lda(Species~., data=iris_train)
   lda.pred=predict(lda.model, iris_test)
   table(lda.pred$class, iris_test$Species)
-
+  mean(lda.pred$class == iris_test$Species)
 
  ### QUADRATIC DISCRIMINANT ANALYSIS ###
-
+  print('-----------------------------')
   print("QUADRATIC DISCRIMINANT ANALYSIS")
 
   qda.model=qda(Species~., data=iris_train)
   qda.pred=predict(qda.model, iris_test)
   table(qda.pred$class, iris_test$Species)
-
+  mean(qda.pred$class == iris_test$Species)
